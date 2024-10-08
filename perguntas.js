a7f8ea7e8bae2082639a52fe42bb16d527c28ec0
FLKYoshi <168427309+FLKYoshi@users.noreply.github.com>
2024-10-08 18:58:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mangá',
+    'Como se lê o mangá?',
+    'Se lê do final para o começo, lendo de direita para esquerda'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tokyo'
+)
+
+criaCartao(
+    'Anime',
+    'O que acontece no final do Boku no Hero?',
+    'O Midoriya Izuku e seus amigos salvam o mundo e hoje em dia o Midoriya Izuku está sem uma individualidade'
+)
+
+criaCartao(
+    'Lingua Japonesa',
+    'Como se diz oi em Japonês?',
+    'Oi em japones é こんにちは (Konnichiwa)'
+)
